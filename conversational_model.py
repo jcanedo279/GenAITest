a64e39f9e992f01373ad4d6c7160e29b274e198f
jcanedo279 <52115366+jcanedo279@users.noreply.github.com>
2023-12-12 15:40:58 -0500

    Add runtime conversational model:  - Fix stopping_criteria token identification.  - 'Fix' some prompt formatting  - Clean up some model generation

--- conversational_model.py
+++ conversational_model.py
@@ -1,63 +1,113 @@
 from transformers import AutoTokenizer, GPTJForCausalLM, StoppingCriteria, StoppingCriteriaList
 import torch
 import config_util
-# from timeit import default_timer as timer
 
 
 DEVICE = config_util.GetModelConfig("Device")
+MAX_RESPONSE_SIZE = 400
+CHARACTER_NAME = "Merlin"
+USER_KEY = "<USER>"
+USER_NAME = "Jcanedo"
+SCENARIO = f"Merlin and {USER_KEY} are on the phone with each other."
+EXAMPLE_PROMPT = "What profound lesson or insight do you believe transcends time and holds eternal significance for humanity?"
+
+# Traits such as "Wise"/"Old" are weighted lightly, 'bake' them in by using Mind+Personality.
+PERSONA = f'''
+    [character("{CHARACTER_NAME}")
+    {{
+    Species("Human")
+    Mind("Wise" + "Old" + "Magical" + "Timeless")
+    Personality("Wise" + "Old" + "Magical" + "Timeless")
+    Body("152cm tall" + "5 foot tall" + "90 pounds")
+    Description("Arthurian legendary wizard" + "Guided Arther to become King" + "advisor for the creation of the Knights of the Round Table")
+    Loves("Hares" + "Magic" + "Knowledge" + "adventure partner Nimue")
+    Gender("Male" + "Asexual" + "Nonsexual")
+    Pronouns("He" + "Him")
+    }}]
+    '''
+
 
 class StoppingCriteriaSub(StoppingCriteria):
 
-    def __init__(self, stop_token_ids):
+    def __init__(self, stop_words, tokenizer):
       super().__init__()
-      self.stop_token_ids = [stop_token_id.to(DEVICE) for stop_token_id in stop_token_ids]
+      self.stop_words = stop_words
+      self.num_tokens_per_stop_word = [len(tokenizer(stop_word, return_tensors='pt', add_special_tokens=False)['input_ids'].squeeze())
+            for stop_word in stop_words]
+      self.tokenizer = tokenizer
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
         gen_tokens = input_ids[0]
-        # Loop over each stop_token_id and check if the last generated tokens match them.
-        for stop_token_id in self.stop_token_ids:
-            stop_token_len = len(stop_token_id)
-            if (len(gen_tokens) > stop_token_len):
-                last_output_id = gen_tokens[-stop_token_len:]
-                if torch.equal(stop_token_id, last_output_id):
-                    return True
+        for stop_word, num_tokens in zip(self.stop_words, self.num_tokens_per_stop_word):
+            # Multiple tokens can have the same translation so we must decode here :<
+            # Also strip because of decoding generating spaces and newlines.
+            if stop_word.strip() == self.tokenizer.decode(gen_tokens[-num_tokens:]).strip():
+                return True
         return False
+
 
 class ConversationalModel:
     model_type_to_name = {
         "Pygmalion": "PygmalionAI/pygmalion-6b"
     }
 
+    def make_model_input(prompt):
+            return f'''
+            {CHARACTER_NAME}'s Persona: {PERSONA}
+            Scenario("{SCENARIO}")
+            <START>
+            You: {prompt}
+            {CHARACTER_NAME}:
+            '''
+
     def __init__(self):
         model_name = ConversationalModel.model_type_to_name["Pygmalion"]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        stop_words = ["|endoftext|", "END_OF_DIALOG"]
-        stop_token_ids = [tokenizer(stop_word, return_tensors='pt', add_special_tokens=False)['input_ids'].squeeze() for stop_word in stop_words]
-        stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stop_token_ids=stop_token_ids)])
-        model = GPTJForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(DEVICE)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
 
-        context = (
-            "[CHARACTER]'s Persona: [A few sentences about the character you want the model to play]"
-            "Merlin, egendary wizard, embodies enigmatic wisdom and magical prowess, timeless aura, bridges the realms of mysticism and sagacity, old, wise"
-            "[DIALOGUE HISTORY]"
-            "You: What profound lesson or insight do you believe transcends time and holds eternal significance for humanity?"
-            "[CHARACTER]:")
-        context_ids = tokenizer(context, return_tensors="pt").input_ids.to(DEVICE)
-        
-        gen_tokens = model.generate(
+        model_input = ConversationalModel.make_model_input(EXAMPLE_PROMPT)
+
+        # We stop generation when the model begins hallucinating the user's response.
+        stop_words = ["You:"]
+        stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stop_words=stop_words, tokenizer=self.tokenizer)])
+        self.model = GPTJForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(DEVICE)
+
+        print('-'*len(EXAMPLE_PROMPT))
+        print("Conversation with 'Merlin':")
+        print(f"You: {EXAMPLE_PROMPT}")
+        self.generate_response(model_input, stopping_criteria)
+
+        while True:
+            user_input = input("You: ")
+            model_input = ConversationalModel.make_model_input(user_input)
+            self.generate_response(model_input, stopping_criteria)
+            
+
+    def generate_response(self, model_input, stopping_criteria):
+        model_input = ( model_input )
+        context_ids = self.tokenizer(model_input, return_tensors="pt").input_ids.to(DEVICE)
+        gen_tokens = self.model.generate(
             context_ids,
+            pad_token_id=self.tokenizer.pad_token_id,
             do_sample=True,
             temperature=0.9,
-            max_length=2048,
+            max_new_tokens=MAX_RESPONSE_SIZE,
             stopping_criteria=stopping_criteria
         )
-        gen_text = tokenizer.batch_decode(gen_tokens)
-        print(gen_text)
+        gen_text = self.tokenizer.batch_decode(gen_tokens)[0]
 
+        # We want to text between '[CHARACTER]' (where the response is) and 'You:' (in our stopping_criteria).
+        gen_response = gen_text.split(f"{CHARACTER_NAME}:")[-1].split("You:")[0]
+        gen_response.replace("<|endoftext|>", "")
+        gen_response.replace(USER_KEY, USER_NAME)
+        # Strip a bunch of misc things such as newlines, spacing, and junk from dailog format and model.
+        gen_response.strip(':\n\"')
+        print(repr(f"{CHARACTER_NAME}: {gen_response}"))
 
 
 def runner():
-    convy = ConversationalModel()
+    ConversationalModel()
 
 if __name__ == "__main__":
     runner()
