88c116afe27a42fd62b8d4754749c7cbf89426eb
jcanedo279 <52115366+jcanedo279@users.noreply.github.com>
2023-12-12 21:03:02 -0500

    Clean up model inputs and model output :>

--- conversational_model.py
+++ conversational_model.py
@@ -5,26 +5,46 @@ import config_util
 
 DEVICE = config_util.GetModelConfig("Device")
 MAX_RESPONSE_SIZE = 400
-CHARACTER_NAME = "Merlin"
 USER_KEY = "<USER>"
 USER_NAME = "Jcanedo"
-SCENARIO = f"Merlin and {USER_KEY} are on the phone with each other."
-EXAMPLE_PROMPT = "What profound lesson or insight do you believe transcends time and holds eternal significance for humanity?"
 
-# Traits such as "Wise"/"Old" are weighted lightly, 'bake' them in by using Mind+Personality.
-PERSONA = f'''
-    [character("{CHARACTER_NAME}")
-    {{
-    Species("Human")
-    Mind("Wise" + "Old" + "Magical" + "Timeless")
-    Personality("Wise" + "Old" + "Magical" + "Timeless")
-    Body("152cm tall" + "5 foot tall" + "90 pounds")
-    Description("Arthurian legendary wizard" + "Guided Arther to become King" + "advisor for the creation of the Knights of the Round Table")
-    Loves("Hares" + "Magic" + "Knowledge" + "adventure partner Nimue")
-    Gender("Male" + "Asexual" + "Nonsexual")
-    Pronouns("He" + "Him")
-    }}]
-    '''
+CHARACTER_NAME = "Merlin"
+CHARACTER_SPECIES = ["Human"]
+CHARACTER_PERSONALITY = ["Wise", "Old", "Magical", "Timeless"]
+CHARACTER_BODY = ["152cm tall", "5 foot tall", "90 pounds"]
+CHARACTER_DESCRIPTIONS = ["Arthurian legendary wizard", "Guided Arther to become King", "advisor for the creation of the Knights of the Round Table"]
+CHARACTER_LOVES = ["Hares", "Magic", "Knowledge", "adventure partner Nimue"]
+CHARACTER_GENDER = ["Male", "Asexual", "Nonsexual"]
+CHARACTER_PRONOUNS = ["He", "Him"]
+
+SCENARIO = f"Merlin and {USER_KEY} are texting each other..."
+STARTING_PROMPT = "What profound lesson or insight do you believe transcends time and holds eternal significance for humanity?"
+
+def PERSONA():
+    def wpp_ize_words(words):
+        return " + ".join([f"\"{word}\"" for word in words])
+    # Personality traits such as "Wise"/"Old" are weighted lightly, 'bake' them in by using Mind+Personality.
+    return f'''
+        [character("{CHARACTER_NAME}")
+        {{
+        Species({wpp_ize_words(CHARACTER_SPECIES)})
+        Mind({wpp_ize_words(CHARACTER_PERSONALITY)})
+        Personality({wpp_ize_words(CHARACTER_PERSONALITY)})
+        Body({wpp_ize_words(CHARACTER_BODY)})
+        Description({wpp_ize_words(CHARACTER_DESCRIPTIONS)})
+        Loves({wpp_ize_words(CHARACTER_LOVES)})
+        Gender({wpp_ize_words(CHARACTER_GENDER)})
+        Pronouns({wpp_ize_words(CHARACTER_PRONOUNS)})
+        }}]'''
+
+def CHARACTER_MODEL_INPUT(prompt):
+    return f'''
+        {CHARACTER_NAME}'s Persona: {PERSONA()}
+        Scenario("{SCENARIO}")
+        <START>
+        You: {prompt}
+        {CHARACTER_NAME}:
+        '''
 
 
 class StoppingCriteriaSub(StoppingCriteria):
@@ -51,36 +71,27 @@ class ConversationalModel:
         "Pygmalion": "PygmalionAI/pygmalion-6b"
     }
 
-    def make_model_input(prompt):
-            return f'''
-            {CHARACTER_NAME}'s Persona: {PERSONA}
-            Scenario("{SCENARIO}")
-            <START>
-            You: {prompt}
-            {CHARACTER_NAME}:
-            '''
-
     def __init__(self):
         model_name = ConversationalModel.model_type_to_name["Pygmalion"]
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         if self.tokenizer.pad_token is None:
             self.tokenizer.pad_token = self.tokenizer.eos_token
 
-        model_input = ConversationalModel.make_model_input(EXAMPLE_PROMPT)
+        model_input = CHARACTER_MODEL_INPUT(STARTING_PROMPT)
 
         # We stop generation when the model begins hallucinating the user's response.
         stop_words = ["You:"]
         stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stop_words=stop_words, tokenizer=self.tokenizer)])
         self.model = GPTJForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(DEVICE)
 
-        print('-'*len(EXAMPLE_PROMPT))
+        print('-'*len(STARTING_PROMPT))
         print("Conversation with 'Merlin':")
-        print(f"You: {EXAMPLE_PROMPT}")
+        print(f"You: {STARTING_PROMPT}")
         self.generate_response(model_input, stopping_criteria)
 
         while True:
             user_input = input("You: ")
-            model_input = ConversationalModel.make_model_input(user_input)
+            model_input = CHARACTER_MODEL_INPUT(user_input)
             self.generate_response(model_input, stopping_criteria)
             
 
@@ -100,10 +111,14 @@ class ConversationalModel:
         # We want to text between '[CHARACTER]' (where the response is) and 'You:' (in our stopping_criteria).
         gen_response = gen_text.split(f"{CHARACTER_NAME}:")[-1].split("You:")[0]
         gen_response.replace("<|endoftext|>", "")
-        gen_response.replace(USER_KEY, USER_NAME)
+        # Replace all instances of USER_KEY with USER_NAME.
+        USER_NAME.join(gen_response.split(USER_KEY))
         # Strip a bunch of misc things such as newlines, spacing, and junk from dailog format and model.
-        gen_response.strip(':\n\"')
-        print(repr(f"{CHARACTER_NAME}: {gen_response}"))
+        garbage_chars = ' :\n\"'
+        gen_response_lines = [response_line.strip(garbage_chars) for response_line in gen_response.strip(garbage_chars).split('\n')]
+        gen_response = "\n  ".join(gen_response_lines)
+
+        print(f"{CHARACTER_NAME}: {gen_response}")
 
 
 def runner():
